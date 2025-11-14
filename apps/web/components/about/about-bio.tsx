1fe15532f9678bb0120cfb97ec9d514f08260174
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2025-11-14 10:37:05 -0500

    Update bio.

--- apps/web/components/about/about-bio.tsx
+++ apps/web/components/about/about-bio.tsx
@@ -14,14 +14,16 @@ export default function AboutBio() {
           <div className="text-gray-400 space-y-4 text-lg leading-relaxed">
             <p>
               Ethan is a full-stack product developer with a focus on FinTech and PropTech platforms
-              that expand economic opportunity for all. Recently, as VP of Engineering at Nada
-              Finance, he built the first home equity investment (HEI) origination platform and its
-              associated retail investment product, Cityfunds. As founding engineer at OwnProp,
-              Ethan led the development of an industry-first tokenization and fractionalization
-              platform for commercial real estate deals. His technical acumen and understanding of
-              emerging technologies' impact on real-world assets position him uniquely to advance
-              innovative solutions. Ethan holds a BA in Economics from the University of Illinois
-              and pursued an MS in Computer Science at the University of Texas.
+              that expand economic opportunity for all. He is currently the Founding Engineer at
+              Castle, building a vendor agnostic Bitcoin treasury management and revenue automation
+              platform for SMBs and CRE. As VP of Engineering at Nada Finance, he built the first
+              home equity investment (HEI) origination platform and its associated retail investment
+              product, Cityfunds. As founding engineer at OwnProp, Ethan led the development of an
+              industry-first tokenization and fractionalization platform for institutional
+              commercial real estate deals. His experience with emerging technologies coupled with
+              an understanding of real-world assets give him a unique background of innovation in
+              financial markets. Ethan holds a BA in Economics from the University of Illinois and
+              pursued an MS in Computer Science at the University of Texas.
             </p>
           </div>
           <section className="py-6">
